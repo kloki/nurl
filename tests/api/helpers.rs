4efdca8687129ff6d38d339d08216bf64b76279c
Koen Klinkers <k.klinkers@gmail.com>
2023-07-07 15:00:36 +0200

    add db to test envarinment

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,7 +1,11 @@
 use nurl::configuration::get_configuration;
+use nurl::configuration::DatabaseSettings;
+use nurl::db::DBClient;
 use nurl::startup::Application;
 use nurl::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use uuid::Uuid;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -18,10 +22,12 @@ pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration");
+        c.database.database_name = Uuid::new_v4().to_string();
         c.application.port = 0;
         c
     };
 
+    configure_database(&configuration.database).await;
     let application = Application::build(configuration.clone())
         .await
         .expect("Failed to build application");
@@ -32,10 +38,31 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         port: application_port,
+        db_client: DBClient::new(&configuration.database),
     }
 }
 
+async fn configure_database(config: &DatabaseSettings) {
+    // Create database
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+}
 pub struct TestApp {
     pub address: String,
     pub port: u16,
+    pub db_client: DBClient,
 }
