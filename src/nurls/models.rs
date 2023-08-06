2e7c65edf7d65b7bf33aacaef0c7156ba23accf8
Koen Klinkers <k.klinkers@gmail.com>
2023-08-06 17:59:58 +0200

    wip

--- src/nurls/models.rs
+++ src/nurls/models.rs
@@ -1,5 +1,30 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use url::Url;
-use uuid::Uuid;
+use uuid::Uuid; // 1.3.0
+
+lazy_static! {
+    static ref BANNER: Regex = Regex::new(r"^[^/\\:]*$").unwrap();
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Nurlet {
+    Url(String),
+    Banner(String),
+}
+
+impl TryFrom<String> for Nurlet {
+    type Error = String;
+    fn try_from(input: String) -> Result<Self, Self::Error> {
+        if input.parse::<Url>().is_ok() {
+            return Ok(Self::Url(input));
+        };
+        if BANNER.is_match(&input) {
+            return Ok(Self::Banner(input));
+        }
+        Err("Not a nurlet".to_string())
+    }
+}
 
 #[derive(Debug)]
 pub struct Nurl {
@@ -34,7 +59,7 @@ impl Nurl {
 
 #[cfg(test)]
 mod tests {
-    use super::Nurl;
+    use super::{Nurl, Nurlet};
     #[tokio::test]
     async fn test_success() {
         let nurl = Nurl::new("test", vec!["http://www.google.nl", "http://facebook.com"]);
@@ -48,5 +73,36 @@ mod tests {
     async fn test_fail() {
         let nurl = Nurl::new("test", vec!["wrong"]);
         assert!(nurl.is_err())
+    }
+
+    #[tokio::test]
+    async fn test_nurlet_parse_banner() {
+        let inputs = vec![
+            "hello".to_string(),
+            "hello again.".to_string(),
+            "hello again 😄".to_string(),
+            "www.google.nl".to_string(),
+        ];
+        for input in inputs {
+            let nurlet: Nurlet = input.clone().try_into().unwrap();
+            assert_eq!(nurlet, Nurlet::Banner(input));
+        }
+    }
+    #[tokio::test]
+    async fn test_nurlet_parse_url() {
+        let inputs = vec!["https://google.nl".to_string()];
+        for input in inputs {
+            let nurlet: Nurlet = input.clone().try_into().unwrap();
+            assert_eq!(nurlet, Nurlet::Url(input));
+        }
+    }
+
+    #[tokio::test]
+    async fn test_nurlet_parse_error() {
+        let inputs = vec!["//".to_string()];
+        for input in inputs {
+            let result: Result<Nurlet, _> = input.clone().try_into();
+            assert!(result.is_err());
+        }
     }
 }
